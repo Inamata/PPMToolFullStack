cb186be022f2931a59c7ebdc670bf325105a417a
CopperZA <46442490+CopperZA@users.noreply.github.com>
2020-01-27 21:08:11 +1300

    Added Functionality

--- ppmtool/src/main/java/com/inamata/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/inamata/ppmtool/services/ProjectService.java
@@ -21,4 +21,14 @@ public class ProjectService {
         }
 
     }
+
+    public Project findProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project==null){
+            throw new ProjectIdException("Project ID does not exist");
+        }
+
+        return projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+    }
 }
