fff1beab90c6dfcc3e4fdc913b21eec5c5831262
CopperZA <46442490+CopperZA@users.noreply.github.com>
2020-01-28 23:45:13 +1300

    Added Bootstrap

---
+++ ppmtool-react-client/src/components/project/ProjectItem.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class ProjectItem extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Project Item</h1>
+            </div>
+        );
+    }
+}
+
+//ProjectItem.propTypes = {};
+
+export default ProjectItem;
